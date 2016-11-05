fe2ca0c727a19ea88ee4ce1305f2cc2744ecbf3e
Sebastien Chauvin <sebastien.chauvin@gmail.com>
2016-11-05 16:25:20 +0100

    do not download file page twice

--- pull.py
+++ pull.py
@@ -8,16 +8,29 @@ import urllib
 import os
 from slackclient import SlackClient
 
+downloaded_lst = []
+try:
+	downloaded_lst = [line.rstrip('\n') for line in open('downloaded_lst.txt', "r")]
+except OSError:
+	print "no download list"
+except IOError:
+	print "no download list"
+
 page = requests.get('http://parapente.ffvl.fr/cfd/liste/2016/last')
 for flightUrl in html.fromstring(page.content).xpath(".//a[contains(@href, '/vol/')]/@href"):
-	tree = html.fromstring(requests.get(flightUrl).content)
-	nameObject = re.match(".*: Le vol de (.*) du (.*)", tree.xpath(".//h1[@class='title']/text()")[0])
-	name = re.sub("/", "_", nameObject.group(2)) + " " + nameObject.group(1) 
-	print name
-	files = tree.xpath(".//a[contains(@href, 'get3d')]/@href")
-	if (len(files) > 0):
+	if not (flightUrl in downloaded_lst):
+		tree = html.fromstring(requests.get(flightUrl).content)
+		nameObject = re.match(".*: Le vol de (.*) du (.*)", tree.xpath(".//h1[@class='title']/text()")[0])
+		name = re.sub("/", "_", nameObject.group(2)) + " " + nameObject.group(1) 
+		print name
 		filename = name + ".kml"
-		if (not Path(filename).is_file()):
-			urllib.urlretrieve(files[0], filename)
-		else:
-			print "skipped"
+		files = tree.xpath(".//a[contains(@href, 'get3d')]/@href")
+		if (len(files) > 0):
+			if (not Path(filename).is_file()):
+				urllib.urlretrieve(files[0], filename)
+			else:
+				print "skipped"
+		with open("downloaded_lst.txt", "a") as f:
+			f.write(flightUrl + "\n")
+	else:
+		print "already donwloaded " + flightUrl
